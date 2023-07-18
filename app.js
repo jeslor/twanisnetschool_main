ca39c48c77c01ee5164cf72c41caa17caaf14401
Jeslor <jbestius@gmail.com>
2023-07-19 01:20:18 +0300

    add the first route

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const express = require('express'),
     app = express();
+
+
+    const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Server is running on port ${port}!`));
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
--- models/users.js
+++ models/users.js
@@ -1,2 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+passportLocalMongoose = require('passport-local-mongoose'),
 Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', UserSchema);
