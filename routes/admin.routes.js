529e3c06dbeb91d09c084d083cf224247939fb73
twaninetschool <142781907+twaninetschool@users.noreply.github.com>
2023-08-28 20:01:12 +0300

    update admin route -search

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -15,13 +15,16 @@ const express = require('express'),
             postAddData,
             getEditData,
             postEditData,
-            getDeleteData} = require('../Controllers/admin.controllers');
+            getDeleteData,
+            getAllUserSearch,
+            getMessageSearch} = require('../Controllers/admin.controllers');
 
 
     router.get('/edituser/:userID/setSubscription', isLoggedIn, getUserEdit);
     router.delete('/deleteuser/:userId',isLoggedIn, isAdministrator, getDeleteUser);
     router.get('/allusers', isLoggedIn, isAdministrator, getAllUsers);
     router.get('/allGuests/messages', isLoggedIn, isAdministrator, getAllGuestMessages);
+    router.get('/allGuests/messages/search',isLoggedIn, isAdministrator, getMessageSearch);
     router.get('/allGuests/messages/:messageId', isLoggedIn, isAdministrator,getEditGuestMessage );
     router.delete('/allGuests/messages/:messageId', isLoggedIn, isAdministrator, getDeleteGuestMessage );
     router.get('/adddata',isLoggedIn, isAdministrator, getAddData);
@@ -29,6 +32,7 @@ const express = require('express'),
     router.get('/editdata/:id',isLoggedIn, isAdministrator, getEditData);
     router.put('/editdata/:id',isLoggedIn, isAdministrator, uploadVideo.single('uploadedvideo'),postEditData);
     router.delete('/deletedata/:videoId',isLoggedIn, isAdministrator, getDeleteData);
+    router.get('/allusers/search',isLoggedIn, isAdministrator, getAllUserSearch);
   
 
 
