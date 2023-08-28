cca58181074b2577f7b8e99e8bfa0cbbcb7606d0
twaninetschool <142781907+twaninetschool@users.noreply.github.com>
2023-08-28 13:27:26 +0300

    Fix the bugs in v2 and also improve the search

---
+++ admin.routes.js
@@ -0,0 +1,39 @@
+const { get } = require('mongoose');
+
+const express = require('express'),
+        router = express.Router(),
+        {upladimage, uploadVideo} = require('../config/videoUploder'),
+
+        {isLoggedIn, isAdministrator} = require('../middleware/userMiddleware'),
+        { getUserEdit,
+            getDeleteUser,
+            getAllUsers,
+            getAllGuestMessages,
+            getEditGuestMessage,
+            getDeleteGuestMessage,
+            getAddData,
+            postAddData,
+            getEditData,
+            postEditData,
+            getDeleteData,
+            getAllUserSearch,
+            getMessageSearch} = require('../Controllers/admin.controllers');
+
+
+    router.get('/edituser/:userID/setSubscription', isLoggedIn, getUserEdit);
+    router.delete('/deleteuser/:userId',isLoggedIn, isAdministrator, getDeleteUser);
+    router.get('/allusers', isLoggedIn, isAdministrator, getAllUsers);
+    router.get('/allGuests/messages', isLoggedIn, isAdministrator, getAllGuestMessages);
+    router.get('/allGuests/messages/search',isLoggedIn, isAdministrator, getMessageSearch);
+    router.get('/allGuests/messages/:messageId', isLoggedIn, isAdministrator,getEditGuestMessage );
+    router.delete('/allGuests/messages/:messageId', isLoggedIn, isAdministrator, getDeleteGuestMessage );
+    router.get('/adddata',isLoggedIn, isAdministrator, getAddData);
+    router.post('/adddata',isLoggedIn, isAdministrator, uploadVideo.single('uploadedvideo'),postAddData);
+    router.get('/editdata/:id',isLoggedIn, isAdministrator, getEditData);
+    router.put('/editdata/:id',isLoggedIn, isAdministrator, uploadVideo.single('uploadedvideo'),postEditData);
+    router.delete('/deletedata/:videoId',isLoggedIn, isAdministrator, getDeleteData);
+    router.get('/allusers/search',isLoggedIn, isAdministrator, getAllUserSearch);
+  
+
+
+module.exports = router;
---
+++ content.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+const contentSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    cost:{
+        type: String,
+        default: 'buy',
+        required: true
+    },
+    level:{
+        type: String,
+        required: true,
+        default: 'all',
+    },
+
+    subject:{
+        type: String,
+        required: true
+    },
+    term:{
+        type: String,
+        required: true
+    },
+    lessonNumber:{
+        type: Number,
+        required: true
+    },
+    topic:{
+        type: String,
+        required: true
+    },
+    videoKey:{
+        type: String,
+    },
+    dateAdded:{
+        type: Date, default: Date.now
+    },
+    videoSize:{
+        type: Number,
+        default: 0,
+    } ,
+    viewedTimes:{
+        type: Number,
+        default: 0,
+        required: true
+    },
+
+}, {timestamps: true}
+);
+
+contentSchema.index({
+    title: 'text',
+    subject: 'text',
+    topic: 'text',
+    level: 'text',
+    term: 'text',
+    cost: 'text',
+    lessonNumber: 'text',
+
+
+});
+// contentSchema.index({subject: 1, topic: 1, level: 1});
+
+module.exports = mongoose.model('Content', contentSchema);
